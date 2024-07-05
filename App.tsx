a22eaed25fa5da4a72556e8a18abbf2dfb8b1f71
NhatLinh <caonhatlinh1312@gmail.com>
2024-07-05 17:30:53 +0700

    update navigator

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import { AppNavigation } from './src/navigation/AppNavigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
+    
     <Provider store={store}>
       <AppNavigation />
     </Provider>
+   
   );
 };
 
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,4 +1,4 @@
-import { NavigationContainer } from "@react-navigation/native"
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -12,17 +12,36 @@ import DetailPlaylist from "../pages/DetailPlaylist";
 // import MV from "../pages/MV";
 // import DetailMV from "../pages/DetailMV";
 import { TestScreen } from "../pages/pagtest";
+import colors from "../assets/colors";
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-
+const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: colors.black,
+    },
+};
 export const AppNavigation = () => {
+
     return (
-        <NavigationContainer>
-            <Stack.Navigator >
+        <NavigationContainer theme={MyTheme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: colors.black,
+                    },
+                    headerTintColor: '#FFF',
+                    headerTitleStyle: {
+                        color: 'white'
+                    }
+                }}
+            >
                 <Stack.Screen name="inapp" component={InappNavigation} options={{ headerShown: false }} />
-                <Stack.Screen name="DetailPlaylist" 
-                    component={DetailPlaylist} 
-                    options={ ({ route }: { route: { params?: { name?: string } } }) => ({ title: route.params?.name, headerShown: true  }) } />
+                <Stack.Screen name="DetailPlaylist"
+                    component={DetailPlaylist}
+                    options={({ route }: { route: { params?: { name?: string } } }) => ({ title: route.params?.name, headerShown: true })} />
             </Stack.Navigator>
         </NavigationContainer>
     );
@@ -30,7 +49,7 @@ export const AppNavigation = () => {
 
 const AuthNavigation = () => {
     return (
-    
+
         <Stack.Navigator >
             <Stack.Screen name="Login" component={LoginScreen} />
         </Stack.Navigator>
@@ -39,20 +58,26 @@ const AuthNavigation = () => {
 
 const InappNavigation = () => {
     return (
-        <Tab.Navigator 
-        screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-                let iconName = '';
-                if(route.name === 'Home') {
-                    iconName = 'home';
-                }
-                return <Icon name={iconName} size={size} color={color} />;
-            },
-           tabBarActiveTintColor: 'tomato',
-           tabBarInactiveTintColor: 'gray',
-        })}
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName = '';
+                    if (route.name === 'Home') {
+                        iconName = 'home';
+                    }
+                    return <Icon name={iconName} size={size} color={color} />;
+                },
+                headerStyle: {
+                    backgroundColor: colors.black,
+                },
+                headerTitleStyle: {
+                    color: 'white'
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}
         >
-        <Tab.Screen name="Home" component={Home}  />
+            <Tab.Screen name="Home" component={Home} />
         </Tab.Navigator>
     );
 }
